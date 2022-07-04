83d9c96cdb90f1bbd04d2fbb00bff2cbe8a44eb3
HamzaZeamari <zeam.hamza@gmail.com>
2022-07-04 22:07:02 +0200

    Add pod animated label to project

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel.text = "⚡️FlashChat"
+        
+        /*
+        titleLabel.text = ""
+        var characterIndex = 0.0
+        let title = "⚡️FlashChat"
+        for letter in title{
+            Timer.scheduledTimer(withTimeInterval: (0.1 * characterIndex), repeats: false, block: {timer in             self.titleLabel.text?.append(letter)
+            })
+            characterIndex += 1
+        }*/
     }
     
 
